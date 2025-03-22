dc81566bc08cb7fff40a91045474c7412b8b5e38
VuPlayBoizz <nguyenbavu1902@gmail.com>
2025-03-22 22:04:05 +0700

    Update Code

--- Infra/Modules/02_aws_ec2/02_server/install/apache-install.sh
+++ Infra/Modules/02_aws_ec2/02_server/install/apache-install.sh
@@ -1,1 +1,8 @@
-
+#! /bin/bash
+cd /home/ubuntu
+sudo apt update -y
+sudo apt install python3 python3-pip -y
+git clone https://github.com/VuPlayBoizz/REST_API.git
+sleep 50
+cd REST_API
+pip3 install -r requirements.txt
