877178b0f60f1c1e88b8a1841b905d1cbec645e7
VuPlayBoizz <nguyenbavu1902@gmail.com>
2025-03-23 15:40:09 +0700

    Update Code

--- Infra/Modules/02_aws_ec2/02_server/install/apache-install.sh
+++ Infra/Modules/02_aws_ec2/02_server/install/apache-install.sh
@@ -4,8 +4,10 @@ sudo apt update -y
 sudo apt install python3 python3-pip -y
 git clone https://github.com/VuPlayBoizz/REST_API.git
 sleep 50
+sudo chown -R ubuntu:ubuntu REST_API/
 cd REST_API/Code
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+sudo apt install python3-venv
+# python3 -m venv venv
+# source venv/bin/activate
+# pip install -r requirements.txt
 
